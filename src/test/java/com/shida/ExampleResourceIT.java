97b729c6f27e6c9abac87d262e5769b33fe0df7e
shida <a980230225@qq.com>
2023-07-25 13:26:41 +0800

    QuakusDemo

---
+++ src/test/java/com/shida/ExampleResourceIT.java
@@ -0,0 +1,8 @@
+package com.shida;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class ExampleResourceIT extends ExampleResourceTest {
+    // Execute the same tests but in packaged mode.
+}
