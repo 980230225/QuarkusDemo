110ba3f8f577fca594eba1b19398909b306ee370
shida <a980230225@qq.com>
2023-08-09 13:29:55 +0800

    QuakusDemo git  push version1.0

--- src/main/java/com/shida/ExampleResource.java
+++ src/main/java/com/shida/ExampleResource.java
@@ -1,16 +1,18 @@
 package com.shida;
 
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/hello")
+@Path("/user")
 public class ExampleResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello from RESTEasy Reactive!";
+        return "Hello from RESTEasy Reactive! version 1.0";
     }
+
 }
